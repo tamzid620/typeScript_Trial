10cb17331a54c87b688a6492deea0736a781aad1
tamzid620 <tamzid620@gmail.com>
2024-09-06 11:46:55 +0600

    array-type file added

---
+++ user-defined-type/array-type.js
@@ -0,0 +1,8 @@
+// let userName : string[] ; or 
+var userNames;
+userNames = ["ami", "kala", "golar", "mala"];
+console.log(userNames[0]);
+userNames.sort();
+console.log(userNames);
+var multiplyTypes;
+multiplyTypes = [1, "amiKala"];
---
+++ user-defined-type/array-type.ts
@@ -0,0 +1,14 @@
+// let userName : string[] ; or 
+let userNames : Array<string> ;
+
+userNames = [ "ami", "kala", "golar", "mala" ] ;
+
+console.log(userNames[0]);
+
+userNames.sort() ;
+console.log(userNames);
+
+let multiplyTypes : (string | number )[];
+multiplyTypes =[1 , "amiKala"] ;
+
+
